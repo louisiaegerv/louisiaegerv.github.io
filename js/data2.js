55ac186fced47017c3dafdaa6ebfd3c02e8f5690
Louis Iaeger <louis.iaeger@gmail.com>
2021-02-23 12:07:33 -0600

    add meme maker to projects list

--- js/data2.js
+++ js/data2.js
@@ -37,97 +37,106 @@ const data = {
     portfolio2: {
         backgroundImg: `img/zipper.png`,
         projects: [
-        {
-            title:          `Breakout`,
-            description:    `Breakout arcade game made w/PhaserJS Framework`,
-            img:            `https://github.com/louisiaegerv/Project-Images/blob/master/breakout.png?raw=true`,
-            link:           `https://louisiaegerv.github.io/breakout/`,
-            tags:           [
-                `JavaScript`, `Game Dev`
-            ]
-        },
-        {
-            title:          `LouTube`,
-            description:    `Simple YouTube Clone made with React`,
-            img:            `https://github.com/louisiaegerv/Project-Images/blob/master/loutube-wide.png?raw=true`,
-            link:           `https://www.youtube.com/watch?v=nr0FQeXYmws&feature=youtu.be`,
-            tags:           [
-                `JavaScript`, `React`
-            ]
-        },
-        {
-            title:          `guestlist`,
-            description:    `Manage your party's RSVP's with this responsive web app made with JavaScript!`,
-            img:            `https://github.com/louisiaegerv/Project-Images/raw/master/GuestListApp.jpg?raw=true`,
-            link:           `https://louisiaegerv.github.io/guestlist/`,
-            tags:           [
-                `JavaScript`, `localstorage`
-            ]
-        },
-        {
-            title:          `stocks-nodejs`,
-            description:    `Get any stocks current NASDAQ price with this NodeJS app.`,
-            img:            `https://github.com/louisiaegerv/Project-Images/blob/master/stock2.png?raw=true`,
-            link:           `https://github.com/louisiaegerv/stocks-nodejs`,
-            tags:           [
-                `JavaScript`, `NodeJS`, `APIs`
-            ]
-        },
-        {
-            title:          `weather-nodejs`,
-            description:    `Get the temp and weather for any ZIP with this NodeJS application.`,
-            img:            `https://raw.githubusercontent.com/louisiaegerv/Project-Images/master/weather2.png`,
-            link:           `https://github.com/louisiaegerv/weather-nodejs`,
-            tags:           [
-                `JavaScript`, `nodeJS`, `APIs`
-            ]
-        },
-        {
-            title:          `quote`,
-            description:    `Random quote generator made with JavaScript & Twitter API.`,
-            img:            `https://github.com/louisiaegerv/Project-Images/raw/master/quoteApp.jpg`,
-            link:           `https://louisiaegerv.github.io/quote`,
-            tags:           [
-                `JavaScript`, `APIs`
-            ]
-        },
-        {
-            title:          `resume`,
-            description:    `My responsive online resume`,
-            img:            `https://github.com/louisiaegerv/resume/raw/master/assets/img/my-resume.png`,
-            link:           `https://louisiaegerv.github.io/resume`,
-            tags:           [
-                `SASS`
-            ]
-        },
-        {
-            title:          `Zephyr layout`,
-            description:    `The responsive and mobile-first template site I created with ReactJS, HTML5, and CSS.`,
-            img:            `https://github.com/louisiaegerv/Project-Images/raw/master/ZephyrSite.jpg`,
-            link:           `https://louisiaegerv.github.io/zephyr/`,
-            tags:           [
-                `JavaScript`, `ReactJS`
-            ]
-        },
-        {
-            title:          `Cobalt layout`,
-            description:    `Another responsive website theme I created using HTML and CSS`,
-            img:            `https://raw.githubusercontent.com/louisiaegerv/Project-Images/master/cobalt.png`,
-            link:           `https://louisiaegerv.github.io/cobalt`,
-            tags:           [
-                `JavaScript`
-            ]
-        },
-        {
-            title:          `noteboard`,
-            description:    `For all those genius ideas you keep forgetting.`,
-            img:            `https://raw.githubusercontent.com/louisiaegerv/Project-Images/master/noteboard.png`,
-            link:           `https://louisiaegerv.github.io/noteboard`,
-            tags:           [
-                `JavaScript`, `jQuery`
-            ]
-        }
-    ]},
+            {
+                title:          `Meme Maker`,
+                description:    `Meme creator made w/React JS and create-react-app`,
+                img:            `https://github.com/louisiaegerv/Project-Images/blob/master/meme-maker-cover-img-portfolio2.png?raw=true`,
+                link:           `https://meme-generator-13660.web.app/`,
+                tags:           [
+                    `JavaScript`, `React    `
+                ]
+            },
+            {
+                title:          `Breakout`,
+                description:    `Breakout arcade game made w/PhaserJS Framework`,
+                img:            `https://github.com/louisiaegerv/Project-Images/blob/master/breakout.png?raw=true`,
+                link:           `https://louisiaegerv.github.io/breakout/`,
+                tags:           [
+                    `JavaScript`, `Game Dev`
+                ]
+            },
+            {
+                title:          `LouTube`,
+                description:    `Simple YouTube Clone made with React`,
+                img:            `https://github.com/louisiaegerv/Project-Images/blob/master/loutube-wide.png?raw=true`,
+                link:           `https://www.youtube.com/watch?v=nr0FQeXYmws&feature=youtu.be`,
+                tags:           [
+                    `JavaScript`, `React`
+                ]
+            },
+            {
+                title:          `guestlist`,
+                description:    `Manage your party's RSVP's with this responsive web app made with JavaScript!`,
+                img:            `https://github.com/louisiaegerv/Project-Images/raw/master/GuestListApp.jpg?raw=true`,
+                link:           `https://louisiaegerv.github.io/guestlist/`,
+                tags:           [
+                    `JavaScript`, `localstorage`
+                ]
+            },
+            {
+                title:          `stocks-nodejs`,
+                description:    `Get any stocks current NASDAQ price with this NodeJS app.`,
+                img:            `https://github.com/louisiaegerv/Project-Images/blob/master/stock2.png?raw=true`,
+                link:           `https://github.com/louisiaegerv/stocks-nodejs`,
+                tags:           [
+                    `JavaScript`, `NodeJS`, `APIs`
+                ]
+            },
+            {
+                title:          `weather-nodejs`,
+                description:    `Get the temp and weather for any ZIP with this NodeJS application.`,
+                img:            `https://raw.githubusercontent.com/louisiaegerv/Project-Images/master/weather2.png`,
+                link:           `https://github.com/louisiaegerv/weather-nodejs`,
+                tags:           [
+                    `JavaScript`, `nodeJS`, `APIs`
+                ]
+            },
+            {
+                title:          `quote`,
+                description:    `Random quote generator made with JavaScript & Twitter API.`,
+                img:            `https://github.com/louisiaegerv/Project-Images/raw/master/quoteApp.jpg`,
+                link:           `https://louisiaegerv.github.io/quote`,
+                tags:           [
+                    `JavaScript`, `APIs`
+                ]
+            },
+            {
+                title:          `resume`,
+                description:    `My responsive online resume`,
+                img:            `https://github.com/louisiaegerv/resume/raw/master/assets/img/my-resume.png`,
+                link:           `https://louisiaegerv.github.io/resume`,
+                tags:           [
+                    `SASS`
+                ]
+            },
+            {
+                title:          `Zephyr Template`,
+                description:    `The responsive and mobile-first template site I created with ReactJS, HTML5, and CSS.`,
+                img:            `https://github.com/louisiaegerv/Project-Images/raw/master/ZephyrSite.jpg`,
+                link:           `https://louisiaegerv.github.io/zephyr/`,
+                tags:           [
+                    `JavaScript`, `ReactJS`
+                ]
+            },
+            {
+                title:          `Cobalt Template`,
+                description:    `Another responsive website theme I created using HTML and CSS`,
+                img:            `https://raw.githubusercontent.com/louisiaegerv/Project-Images/master/cobalt.png`,
+                link:           `https://louisiaegerv.github.io/cobalt`,
+                tags:           [
+                    `JavaScript`
+                ]
+            },
+            {
+                title:          `noteboard`,
+                description:    `For all those genius ideas you keep forgetting.`,
+                img:            `https://raw.githubusercontent.com/louisiaegerv/Project-Images/master/noteboard.png`,
+                link:           `https://louisiaegerv.github.io/noteboard`,
+                tags:           [
+                    `JavaScript`, `jQuery`
+                ]
+            }
+        ]},
     contact: {
         email: `louis.iaeger@yahoo.com`
     },
