729d9dec57d01f34d8c2b2abc5c86e76c1b4be7c
Louis Iaeger <louis.iaeger@gmail.com>
2021-04-03 11:17:09 -0500

    Add new React portfolio projects

--- js/data2.js
+++ js/data2.js
@@ -1,180 +1,195 @@
 const data = {
-    nav: {
-        title: {
-            text: `Louis Iaeger V`,
-            link: `#app`
-        },
-        navLinks: [
-            {
-                name:   `Home`,
-                url:    `#app`
-            },
-            {
-                name:   `About`,
-                url:    `#about`
-            },
-            {
-                name:   `Projects`,
-                url:    `#projects`
-            },
-            {
-                name:   `Contact`,
-                url:    `#contact`
-            }
-        ]
+  nav: {
+    title: {
+      text: `Louis Iaeger V`,
+      link: `#app`,
     },
-    jumbo: {
-        text: `Hi, I'm Louis. I design & build websites.`,
-        buttonText: `Yes, I'm available for hire.`,
-        buttonLink: `#contact`,
-        scrollDownLink: '#about',
-        img: `img/desk4.jpg`
+    navLinks: [
+      {
+        name: `Home`,
+        url: `#app`,
+      },
+      {
+        name: `About`,
+        url: `#about`,
+      },
+      {
+        name: `Projects`,
+        url: `#projects`,
+      },
+      {
+        name: `Contact`,
+        url: `#contact`,
+      },
+    ],
+  },
+  jumbo: {
+    text: `Hi, I'm Louis. I design & build websites.`,
+    buttonText: `Yes, I'm available for hire.`,
+    buttonLink: `#contact`,
+    scrollDownLink: '#about',
+    img: `img/desk4.jpg`,
+  },
+  about: {
+    img: `img/me.jpg`,
+    aboutText: `Highly-driven Full-Stack software developer and software engineer with more than five years experience producing solutions for large corporations and local businesses. Proven skillset for designing, testing, and launching responsive software.`,
+  },
+  portfolio2: {
+    backgroundImg: `img/zipper.png`,
+    projects: [
+      {
+        title: `React Todos`,
+        description: `Todo web app built with React. Add, edit, complete, and delete todos.`,
+        img: `https://github.com/louisiaegerv/Project-Images/raw/master/React-todo-image.png?raw=true`,
+        link: `https://react-todo-ljfiv.netlify.app/`,
+        tags: [`JavaScript`, `React`],
+      },
+      {
+        title: `React Menu`,
+        description: `A restaurant menu web app made with React`,
+        img: `https://github.com/louisiaegerv/Project-Images/raw/master/React-menu-image.png?raw=true`,
+        link: `https://react-menu-ljfiv.netlify.app/`,
+        tags: [`JavaScript`, `React`],
+      },
+      {
+        title: `React Resume`,
+        description: `A simple resume web app component made with React.`,
+        img: `https://github.com/louisiaegerv/Project-Images/raw/master/React-Resume-img.png?raw=true`,
+        link: `https://react-resume-ljfiv.netlify.app/`,
+        tags: [`JavaScript`, `React`],
+      },
+      {
+        title: `React FAQs`,
+        description: `Cryptocurrency-themed FAQ web app component made with React.`,
+        img: `https://github.com/louisiaegerv/Project-Images/raw/master/React-crypto-faqs.png?raw=true`,
+        link: `https://react-crypto-faqs-ljfiv.netlify.app/`,
+        tags: [`JavaScript`, `React`],
+      },
+      {
+        title: `React Tours`,
+        description: `World Tour Business web app made with React`,
+        img: `https://github.com/louisiaegerv/Project-Images/raw/master/react-tours-image.png?raw=true`,
+        link: `https://react-tours-ljfiv.netlify.app/`,
+        tags: [`JavaScript`, `React`],
+      },
+      {
+        title: `Meme Maker`,
+        description: `Meme creator made w/React JS and create-react-app`,
+        img: `https://github.com/louisiaegerv/Project-Images/blob/master/meme-maker-cover-img-portfolio2.png?raw=true`,
+        link: `https://meme-generator-13660.web.app/`,
+        tags: [`JavaScript`, `React    `],
+      },
+      {
+        title: `Breakout`,
+        description: `Breakout arcade game made w/PhaserJS Framework`,
+        img: `https://github.com/louisiaegerv/Project-Images/blob/master/breakout.png?raw=true`,
+        link: `https://louisiaegerv.github.io/breakout/`,
+        tags: [`JavaScript`, `Game Dev`],
+      },
+      {
+        title: `LouTube`,
+        description: `Simple YouTube Clone made with React`,
+        img: `https://github.com/louisiaegerv/Project-Images/blob/master/loutube-wide.png?raw=true`,
+        link: `https://www.youtube.com/watch?v=nr0FQeXYmws&feature=youtu.be`,
+        tags: [`JavaScript`, `React`],
+      },
+      {
+        title: `guestlist`,
+        description: `Manage your party's RSVP's with this responsive web app made with JavaScript!`,
+        img: `https://github.com/louisiaegerv/Project-Images/raw/master/GuestListApp.jpg?raw=true`,
+        link: `https://louisiaegerv.github.io/guestlist/`,
+        tags: [`JavaScript`, `localstorage`],
+      },
+      {
+        title: `stocks-nodejs`,
+        description: `Get any stocks current NASDAQ price with this NodeJS app.`,
+        img: `https://github.com/louisiaegerv/Project-Images/blob/master/stock2.png?raw=true`,
+        link: `https://github.com/louisiaegerv/stocks-nodejs`,
+        tags: [`JavaScript`, `NodeJS`, `APIs`],
+      },
+      {
+        title: `weather-nodejs`,
+        description: `Get the temp and weather for any ZIP with this NodeJS application.`,
+        img: `https://raw.githubusercontent.com/louisiaegerv/Project-Images/master/weather2.png`,
+        link: `https://github.com/louisiaegerv/weather-nodejs`,
+        tags: [`JavaScript`, `nodeJS`, `APIs`],
+      },
+      {
+        title: `quote`,
+        description: `Random quote generator made with JavaScript & Twitter API.`,
+        img: `https://github.com/louisiaegerv/Project-Images/raw/master/quoteApp.jpg`,
+        link: `https://louisiaegerv.github.io/quote`,
+        tags: [`JavaScript`, `APIs`],
+      },
+      {
+        title: `resume`,
+        description: `My responsive online resume`,
+        img: `https://github.com/louisiaegerv/resume/raw/master/assets/img/my-resume.png`,
+        link: `https://louisiaegerv.github.io/resume`,
+        tags: [`SASS`],
+      },
+      {
+        title: `Zephyr Template`,
+        description: `The responsive and mobile-first template site I created with ReactJS, HTML5, and CSS.`,
+        img: `https://github.com/louisiaegerv/Project-Images/raw/master/ZephyrSite.jpg`,
+        link: `https://louisiaegerv.github.io/zephyr/`,
+        tags: [`JavaScript`, `ReactJS`],
+      },
+      {
+        title: `Cobalt Template`,
+        description: `Another responsive website theme I created using HTML and CSS`,
+        img: `https://raw.githubusercontent.com/louisiaegerv/Project-Images/master/cobalt.png`,
+        link: `https://louisiaegerv.github.io/cobalt`,
+        tags: [`JavaScript`],
+      },
+      {
+        title: `noteboard`,
+        description: `For all those genius ideas you keep forgetting.`,
+        img: `https://raw.githubusercontent.com/louisiaegerv/Project-Images/master/noteboard.png`,
+        link: `https://louisiaegerv.github.io/noteboard`,
+        tags: [`JavaScript`, `jQuery`],
+      },
+    ],
+  },
+  contact: {
+    email: `louis.iaeger@yahoo.com`,
+  },
+  social: [
+    {
+      name: 'twitter',
+      shortlink: 'twitter.com/theCodeslayer/',
+      fullLink: 'https://twitter.com/theCodeslayer/',
+      img: '',
+      fontAwesome: true,
     },
-    about: {
-        img: `img/me.jpg`,
-        aboutText: `Passionate programmer pursuing a career path in web/app development and mixed reality technologies. I bring a strong background in UX and attention to detail that will increase engagement between your company and its target audience.`
+    {
+      name: 'github',
+      shortlink: 'github.com/louisiaegerv/',
+      fullLink: 'https://github.com/louisiaegerv/',
+      img: '',
+      fontAwesome: true,
     },
-    portfolio2: {
-        backgroundImg: `img/zipper.png`,
-        projects: [
-            {
-                title:          `Meme Maker`,
-                description:    `Meme creator made w/React JS and create-react-app`,
-                img:            `https://github.com/louisiaegerv/Project-Images/blob/master/meme-maker-cover-img-portfolio2.png?raw=true`,
-                link:           `https://meme-generator-13660.web.app/`,
-                tags:           [
-                    `JavaScript`, `React    `
-                ]
-            },
-            {
-                title:          `Breakout`,
-                description:    `Breakout arcade game made w/PhaserJS Framework`,
-                img:            `https://github.com/louisiaegerv/Project-Images/blob/master/breakout.png?raw=true`,
-                link:           `https://louisiaegerv.github.io/breakout/`,
-                tags:           [
-                    `JavaScript`, `Game Dev`
-                ]
-            },
-            {
-                title:          `LouTube`,
-                description:    `Simple YouTube Clone made with React`,
-                img:            `https://github.com/louisiaegerv/Project-Images/blob/master/loutube-wide.png?raw=true`,
-                link:           `https://www.youtube.com/watch?v=nr0FQeXYmws&feature=youtu.be`,
-                tags:           [
-                    `JavaScript`, `React`
-                ]
-            },
-            {
-                title:          `guestlist`,
-                description:    `Manage your party's RSVP's with this responsive web app made with JavaScript!`,
-                img:            `https://github.com/louisiaegerv/Project-Images/raw/master/GuestListApp.jpg?raw=true`,
-                link:           `https://louisiaegerv.github.io/guestlist/`,
-                tags:           [
-                    `JavaScript`, `localstorage`
-                ]
-            },
-            {
-                title:          `stocks-nodejs`,
-                description:    `Get any stocks current NASDAQ price with this NodeJS app.`,
-                img:            `https://github.com/louisiaegerv/Project-Images/blob/master/stock2.png?raw=true`,
-                link:           `https://github.com/louisiaegerv/stocks-nodejs`,
-                tags:           [
-                    `JavaScript`, `NodeJS`, `APIs`
-                ]
-            },
-            {
-                title:          `weather-nodejs`,
-                description:    `Get the temp and weather for any ZIP with this NodeJS application.`,
-                img:            `https://raw.githubusercontent.com/louisiaegerv/Project-Images/master/weather2.png`,
-                link:           `https://github.com/louisiaegerv/weather-nodejs`,
-                tags:           [
-                    `JavaScript`, `nodeJS`, `APIs`
-                ]
-            },
-            {
-                title:          `quote`,
-                description:    `Random quote generator made with JavaScript & Twitter API.`,
-                img:            `https://github.com/louisiaegerv/Project-Images/raw/master/quoteApp.jpg`,
-                link:           `https://louisiaegerv.github.io/quote`,
-                tags:           [
-                    `JavaScript`, `APIs`
-                ]
-            },
-            {
-                title:          `resume`,
-                description:    `My responsive online resume`,
-                img:            `https://github.com/louisiaegerv/resume/raw/master/assets/img/my-resume.png`,
-                link:           `https://louisiaegerv.github.io/resume`,
-                tags:           [
-                    `SASS`
-                ]
-            },
-            {
-                title:          `Zephyr Template`,
-                description:    `The responsive and mobile-first template site I created with ReactJS, HTML5, and CSS.`,
-                img:            `https://github.com/louisiaegerv/Project-Images/raw/master/ZephyrSite.jpg`,
-                link:           `https://louisiaegerv.github.io/zephyr/`,
-                tags:           [
-                    `JavaScript`, `ReactJS`
-                ]
-            },
-            {
-                title:          `Cobalt Template`,
-                description:    `Another responsive website theme I created using HTML and CSS`,
-                img:            `https://raw.githubusercontent.com/louisiaegerv/Project-Images/master/cobalt.png`,
-                link:           `https://louisiaegerv.github.io/cobalt`,
-                tags:           [
-                    `JavaScript`
-                ]
-            },
-            {
-                title:          `noteboard`,
-                description:    `For all those genius ideas you keep forgetting.`,
-                img:            `https://raw.githubusercontent.com/louisiaegerv/Project-Images/master/noteboard.png`,
-                link:           `https://louisiaegerv.github.io/noteboard`,
-                tags:           [
-                    `JavaScript`, `jQuery`
-                ]
-            }
-        ]},
-    contact: {
-        email: `louis.iaeger@yahoo.com`
+    {
+      name: 'linkedin',
+      shortlink: 'linkedin.com/in/louislinkedin',
+      fullLink: 'https://www.linkedin.com/in/louislinkedin',
+      img: '',
+      fontAwesome: true,
     },
-    social: [
-        {
-            name: 'twitter',
-            shortlink: 'twitter.com/theCodeslayer/',
-            fullLink: 'https://twitter.com/theCodeslayer/',
-            img: '',
-            fontAwesome: true
-        },
-        {
-            name: 'github',
-            shortlink: 'github.com/louisiaegerv/',
-            fullLink: 'https://github.com/louisiaegerv/',
-            img: '',
-            fontAwesome: true
-        },
-        {
-            name:       'linkedin',
-            shortlink:  'linkedin.com/in/louislinkedin',
-            fullLink:   'https://www.linkedin.com/in/louislinkedin',
-            img:        '',
-            fontAwesome: true
-        },
-        {
-            name:       'Codecademy',
-            shortlink:  'codecademy.com/megapandalord',
-            fullLink:   'https://www.codecademy.com/megapandalord',
-            img:        'https://louisiaegerv.github.io/resume/assets/img/cc2w.png',
-            fontAwesome: false
-        },
-        {
-            name:       'Team Treehouse',
-            shortlink:  'teamtreehouse.com/louisiaeger',
-            fullLink:   'https://teamtreehouse.com/louisiaeger',
-            img:        'https://raw.githubusercontent.com/louisiaegerv/Project-Images/master/treehouse.png',
-            fontAwesome: false
-        }
-    ]
-};
+    {
+      name: 'Codecademy',
+      shortlink: 'codecademy.com/megapandalord',
+      fullLink: 'https://www.codecademy.com/megapandalord',
+      img: 'https://louisiaegerv.github.io/resume/assets/img/cc2w.png',
+      fontAwesome: false,
+    },
+    {
+      name: 'Team Treehouse',
+      shortlink: 'teamtreehouse.com/louisiaeger',
+      fullLink: 'https://teamtreehouse.com/louisiaeger',
+      img:
+        'https://raw.githubusercontent.com/louisiaegerv/Project-Images/master/treehouse.png',
+      fontAwesome: false,
+    },
+  ],
+}
--- js/index.js
+++ js/index.js
@@ -1,127 +1,133 @@
-(() => {
-    /* DOM Section variables */
-    const app = document.querySelector('#app');
-    const headerSection = document.querySelector('header');
-    const jumboSection = document.querySelector('#jumbo');
-    const aboutSection = document.querySelector('#about');
-    const portfolioSection = document.querySelector('#projects');
-    const contactSection = document.querySelector('#contact');
-    const footerSection = app.lastElementChild;
-    /* Turn data keys into variables */
-    const nav = data.nav;
-    const jumbo = data.jumbo;
-    const about = data.about;
-    const portfolio = data.portfolio2;
-    const contact = data.contact;
-    const social = data.social;
-    /* Functions */
-    const createLinkLI = (linkTxt, link) => {
-        const a = document.createElement('a');
-        const li = document.createElement('li');
-        li.innerHTML = linkTxt;
-        a.href = link;
-        a.appendChild(li);
-        return a;
-    };
+;(() => {
+  /* DOM Section variables */
+  const app = document.querySelector('#app')
+  const headerSection = document.querySelector('header')
+  const jumboSection = document.querySelector('#jumbo')
+  const aboutSection = document.querySelector('#about')
+  const portfolioSection = document.querySelector('#projects')
+  const contactSection = document.querySelector('#contact')
+  const footerSection = app.lastElementChild
+  /* Turn data keys into variables */
+  const nav = data.nav
+  const jumbo = data.jumbo
+  const about = data.about
+  const portfolio = data.portfolio2
+  const contact = data.contact
+  const social = data.social
+  /* Functions */
+  const createLinkLI = (linkTxt, link) => {
+    const a = document.createElement('a')
+    const li = document.createElement('li')
+    li.innerHTML = linkTxt
+    a.href = link
+    a.appendChild(li)
+    return a
+  }
 
-    /* Header section */
-    const title = createLinkLI(nav.title.text, nav.title.link);
-    title.classList.add('title');
-    headerSection.firstElementChild.appendChild(title);
+  /* Header section */
+  const title = createLinkLI(nav.title.text, nav.title.link)
+  title.classList.add('title')
+  headerSection.firstElementChild.appendChild(title)
 
-    const navButton = document.createElement('p');
-    navButton.innerHTML = `&#9776;`;
-    navButton.classList.add('navButton');
-    headerSection.firstElementChild.appendChild(navButton);
+  const navButton = document.createElement('p')
+  navButton.innerHTML = `&#9776;`
+  navButton.classList.add('navButton')
+  headerSection.firstElementChild.appendChild(navButton)
 
-    const headerList = document.createElement('ul');
-    headerList.classList.add("headerList");
-    navButton.addEventListener('click', () => {
-        headerList.classList.toggle('active');
-    });
-    const headerLinks = nav.navLinks;
-    headerLinks.forEach(link => {
-        const li = createLinkLI(link.name, link.url);
-        li.addEventListener('click', () => {
-            headerList.classList.toggle('active');
-        });
-        headerList.appendChild(li);
-    });
-    headerSection.firstElementChild.appendChild(headerList);
+  const headerList = document.createElement('ul')
+  headerList.classList.add('headerList')
+  navButton.addEventListener('click', () => {
+    headerList.classList.toggle('active')
+  })
+  const headerLinks = nav.navLinks
+  headerLinks.forEach((link) => {
+    const li = createLinkLI(link.name, link.url)
+    li.addEventListener('click', () => {
+      headerList.classList.toggle('active')
+    })
+    headerList.appendChild(li)
+  })
+  headerSection.firstElementChild.appendChild(headerList)
 
+  /* Jumbotron section */
+  jumboSection.style.backgroundImage =
+    `linear-gradient( rgba(0, 0, 0, .8), rgba(255, 255, 255, .4)), url(` +
+    data.jumbo.img +
+    `)`
+  const jumboContentContainer = document.createElement('div')
+  jumboContentContainer.classList.add('jumboContentContainer')
+  const jumboText = document.createElement('h1')
+  jumboText.textContent = jumbo.text
+  jumboContentContainer.appendChild(jumboText)
+  const jumboButton = document.createElement('a')
+  jumboButton.textContent = jumbo.buttonText
+  jumboButton.href = jumbo.buttonLink
+  jumboContentContainer.appendChild(jumboButton)
+  jumboSection.appendChild(jumboContentContainer)
 
+  const scrollDown = document.createElement('a')
+  scrollDown.href = jumbo.scrollDownLink
+  scrollDown.classList.add('scrollDown')
+  scrollDown.innerHTML =
+    '<i class="fa fa-angle-down fa-2x" aria-hidden="true"></i>'
+  jumboSection.appendChild(scrollDown)
 
-    /* Jumbotron section */
-    jumboSection.style.backgroundImage = `linear-gradient( rgba(0, 0, 0, .8), rgba(255, 255, 255, .4)), url(` + data.jumbo.img + `)`;
-    const jumboContentContainer = document.createElement('div');
-    jumboContentContainer.classList.add('jumboContentContainer');
-    const jumboText = document.createElement('h1');
-    jumboText.textContent = jumbo.text;
-    jumboContentContainer.appendChild(jumboText);
-    const jumboButton = document.createElement('a');
-    jumboButton.textContent = jumbo.buttonText;
-    jumboButton.href = jumbo.buttonLink;
-    jumboContentContainer.appendChild(jumboButton);
-    jumboSection.appendChild(jumboContentContainer);
+  /* About section */
+  const aboutImg = document.createElement('img')
+  aboutImg.src = about.img
+  aboutSection.firstElementChild.firstElementChild.appendChild(aboutImg)
+  const aboutTxt = document.createElement('p')
+  aboutTxt.textContent = about.aboutText
+  aboutSection.firstElementChild.lastElementChild.appendChild(aboutTxt)
 
-    const scrollDown = document.createElement('a');
-    scrollDown.href = jumbo.scrollDownLink;
-    scrollDown.classList.add('scrollDown');
-    scrollDown.innerHTML = '<i class="fa fa-angle-down fa-2x" aria-hidden="true"></i>';
-    jumboSection.appendChild(scrollDown);
-
-
-    /* About section */
-    const aboutImg = document.createElement('img');
-    aboutImg.src = about.img;
-    aboutSection.firstElementChild.firstElementChild.appendChild(aboutImg);
-    const aboutTxt = document.createElement('p');
-    aboutTxt.textContent = about.aboutText;
-    aboutSection.firstElementChild.lastElementChild.appendChild(aboutTxt);
-
-    /* Portfolio Section */
-    portfolioSection.style.background = `url(` + portfolio.backgroundImg + `)  center center fixed`;
-    portfolio.projects.forEach(project => {
-        const projContainer = document.createElement('div');
-        const proj = document.createElement('div');
-        const imgLink = document.createElement('a');
-        proj.style.background = 'url(' + project.img + ') no-repeat center';
-        proj.style.transition = 'all 0.2s ease';
-        projContainer.classList.add('col-md-4', 'col-sm-6', 'projContainer');
-        proj.style.backgroundSize = 'cover';
-        imgLink.textContent = project.title;
-        imgLink.href = project.link;
-        imgLink.target = '_blank';
-        $(imgLink).hover(() => {
-            imgLink.parentElement.style.transform = "scale(1.1)";
-        }, () => {
-            imgLink.parentElement.style.transform = "scale(1)";
-        });
-        proj.appendChild(imgLink);
-        projContainer.appendChild(proj);
-        portfolioSection.firstElementChild.children[1].appendChild(projContainer);
-    });
-    /* Contact Section */
-    /* Footer Section */
-    const year = new Date().getFullYear();
-    let footerCopyright = document.createElement('p');
-    footerCopyright.textContent = `Handmade by me © ${year}.`;
-    footerSection.firstElementChild.appendChild(footerCopyright);
-    social.forEach(socialAccount => {
-        const socialLink = document.createElement('a');
-        if (socialAccount.fontAwesome === true) {
-            const faIcon = '<i class="fa fa-' + socialAccount.name + ' fa-2x" aria-hidden="true"></i>';
-            socialLink.innerHTML = faIcon;
-        }
-        else if (socialAccount.img !== '') {
-            const iconImg = '<img src="' + socialAccount.img + '">';
-            socialLink.innerHTML = iconImg;
-        }
-        else {
-            socialLink.textContent = socialAccount.name;
-        }
-        socialLink.href = socialAccount.fullLink;
-        socialLink.target = '_blank';
-        footerSection.lastElementChild.appendChild(socialLink);
-    });
-})();
+  /* Portfolio Section */
+  portfolioSection.style.background =
+    `url(` + portfolio.backgroundImg + `)  center center fixed`
+  portfolio.projects.forEach((project) => {
+    const projContainer = document.createElement('div')
+    const proj = document.createElement('div')
+    const imgLink = document.createElement('a')
+    proj.style.background = 'url(' + project.img + ') no-repeat center'
+    proj.style.transition = 'all 0.2s ease'
+    projContainer.classList.add('col-md-4', 'col-sm-6', 'projContainer')
+    proj.style.backgroundSize = 'cover'
+    imgLink.textContent = project.title
+    imgLink.href = project.link
+    imgLink.target = '_blank'
+    $(imgLink).hover(
+      () => {
+        imgLink.parentElement.style.transform = 'scale(1.1)'
+      },
+      () => {
+        imgLink.parentElement.style.transform = 'scale(1)'
+      }
+    )
+    proj.appendChild(imgLink)
+    projContainer.appendChild(proj)
+    portfolioSection.firstElementChild.children[1].appendChild(projContainer)
+  })
+  /* Contact Section */
+  /* Footer Section */
+  const year = new Date().getFullYear()
+  let footerCopyright = document.createElement('p')
+  footerCopyright.textContent = `Handmade by me © ${year}.`
+  footerSection.firstElementChild.appendChild(footerCopyright)
+  social.forEach((socialAccount) => {
+    const socialLink = document.createElement('a')
+    if (socialAccount.fontAwesome === true) {
+      const faIcon =
+        '<i class="fa fa-' +
+        socialAccount.name +
+        ' fa-2x" aria-hidden="true"></i>'
+      socialLink.innerHTML = faIcon
+    } else if (socialAccount.img !== '') {
+      const iconImg = '<img src="' + socialAccount.img + '">'
+      socialLink.innerHTML = iconImg
+    } else {
+      socialLink.textContent = socialAccount.name
+    }
+    socialLink.href = socialAccount.fullLink
+    socialLink.target = '_blank'
+    footerSection.lastElementChild.appendChild(socialLink)
+  })
+})()
